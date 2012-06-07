1054cefdd861875131d5bfee5cfc15fda32c1805
deanonishi <dean@rightscale.com>
2012-06-07 23:36:00 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/intermediate/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: intermediate
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello World! You should try Chef with RightScale!"
+
+
