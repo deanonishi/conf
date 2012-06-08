223ae4f127ddb5c39d2ed07c2e9f26fc0c1fe9fe
deanonishi <dean@rightscale.com>
2012-06-08 07:20:02 +0000

    Converted the hobby attribute into an input

--- cookbooks/about_me/attributes/me.rb
+++ cookbooks/about_me/attributes/me.rb
@@ -1,4 +1,4 @@
-set[:about_me][:first_name] = "<YOUR_NAME>"
+set[:about_me][:first_name] = "Dean"
 set_unless[:about_me][:favorite][:food] = "pizza"
 set_unless[:about_me][:favorite][:drink] = "water"
 set_unless[:about_me][:favorite][:hobby] = "running"
--- cookbooks/about_me/metadata.rb
+++ cookbooks/about_me/metadata.rb
@@ -9,6 +9,11 @@ depends "rs_utils"
 
 recipe "about_me::default","Prints my first name and several of my favorite things as output."
 
-
+# Required #
+attribute "about_me/favorite/hobby",
+   :display_name => "Hobby",
+      :description => "My favorite hobby.",
+         :required => "required",
+            :recipes => ["about_me::default"]
 
 
